af0b65ed59f9430a69715a4a74de542fe828ee76
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-06-24 22:19:40 -0300

    ✨ Resposive layout in categories page

--- responsive_scripts.js
+++ responsive_scripts.js
@@ -1,9 +1,15 @@
 const btnMobile = document.getElementById('btn-mobile');
 
+let aux = true;
 function toggleMenu(event) {
+    if (document.body.style.overflow !== 'hidden') { document.body.style.overflow = 'hidden' } else { document.body.style.overflow = 'initial' }
     if (event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
+
+
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+// document.body.style.overflow = aux ? "hidden" : "initial"
+
